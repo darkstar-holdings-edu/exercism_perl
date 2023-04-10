70a88dde34ffd514f867aeaae754ced664e2c79f
David Betz <hashref@gmail.com>
2023-04-10 14:53:09 -0700

    Add solution for rna-transcription exercise

--- rna-transcription/RNA.pm
+++ rna-transcription/RNA.pm
@@ -7,9 +7,24 @@ use feature qw<say>;
 use Exporter qw<import>;
 our @EXPORT_OK = qw<to_rna>;
 
+my %DNA_TO_RNA_MAP = (
+  'G' => 'C',
+  'C' => 'G',
+  'T' => 'A',
+  'A' => 'U'
+);
+
 sub to_rna {
-    my ($dna) = @_;
-    return undef;
+  my ($dna) = @_;
+
+  my $rna = '';
+
+  for my $i ( 0 .. length($dna) - 1 ) {
+    my $char = substr( $dna, $i, 1 );
+    $rna .= $DNA_TO_RNA_MAP{$char} || $char;
+  }
+
+  return $rna;
 }
 
 1;
