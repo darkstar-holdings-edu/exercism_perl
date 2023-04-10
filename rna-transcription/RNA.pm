775ebba68aef9913082699b140c89489a67273bc
David Betz <hashref@gmail.com>
2023-04-10 14:40:08 -0700

    Init RNA exercise

---
+++ rna-transcription/RNA.pm
@@ -0,0 +1,15 @@
+package RNA;
+
+use strict;
+use warnings;
+use feature qw<say>;
+
+use Exporter qw<import>;
+our @EXPORT_OK = qw<to_rna>;
+
+sub to_rna {
+    my ($dna) = @_;
+    return undef;
+}
+
+1;
